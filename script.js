019eec59834398f248b6baeccf6bdccb55c59828
VusiDev <68845231+vusidev@users.noreply.github.com>
2021-04-07 23:33:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var menuIcon = document.getElementById("menu");
+var menu = document.getElementById("navBar");
+function ToogleMenu(){    if(menuIcon.textContent == '=/\\=')    {        menuIcon.textContent = '=\\/=';        menu.style.display = 'none';   
+ }    else    {    
+    menuIcon.textContent = '=/\\=';        menu.style.display = 'block';   
+ }    
+}
+menuIcon.addEventListener("click", ToogleMenu);
